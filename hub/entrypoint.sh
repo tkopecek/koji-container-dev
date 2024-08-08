7c68e74bfbdf18428d152d8440ed1c4accfd5417
Tomas Kopecek <tkopecek@redhat.com>
2024-08-08 12:32:48 +0200

    update for podman5 and pasta

--- hub/entrypoint.sh
+++ hub/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# No need to edit these files if using docker compose v2
-if [ "${ENTRYPOINT_SKIP_CONFIG}" != "True" ]
-then
-    sed -e "s/DBHost.*/DBHost = $HOSTIP/" -i /etc/koji-hub/hub.conf
-    sed -e "s/KojiHubURL.*/KojiHubURL = http:\/\/$HOSTIP:8080\/kojihub/" -i /etc/kojiweb/web.conf
-    sed -e "s/KojiFilesURL.*/KojiFilesURL = http:\/\/$HOSTIP:8080\/kojifiles/" -i /etc/kojiweb/web.conf
-fi
-
 httpd &
 sleep 1
 tail -f /var/log/httpd/ssl_error_log
